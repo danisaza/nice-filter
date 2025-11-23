7384325f8655e322202024092e1baccd53a32c1b
Dan Isaza <dan@isaza.dev>
2025-11-22 22:46:04 -0800

    update vite config to enable profiling builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			// alias the client entry to the profiling build
+			"react-dom/client": "react-dom/profiling",
+			"scheduler/tracing": "scheduler/tracing-profiling",
+		},
+	},
+});
