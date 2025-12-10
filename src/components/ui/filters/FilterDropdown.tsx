5fb23ec7c75543682c886dcec3a529cc7a6437c3
Dan Isaza <dan@isaza.dev>
2025-12-10 13:24:19 -0800

    wraps dropdown menu in portal

--- src/components/ui/filters/FilterDropdown.tsx
+++ src/components/ui/filters/FilterDropdown.tsx
@@ -130,61 +130,63 @@ const FilterDropdown = ({
 					Filter
 				</Button>
 			</Toolbar.Button>
-			<DropdownMenu.Content
-				align="start"
-				className="border border-gray-300 w-[260px] rounded bg-white shadow-md data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=top]:animate-slideDownAndFade"
-				sideOffset={25}
-				alignOffset={0}
-				onCloseAutoFocus={(event) => {
-					event.preventDefault();
-					buttonRef.current?.focus();
-				}}
-			>
-				<Input
-					id="search-input"
-					onKeyDown={(keyDownEvent) => {
-						if (keyDownEvent.key === "ArrowDown") {
-							keyDownEvent.preventDefault();
-							firstSubTriggerRef.current?.focus();
-						} else if (keyDownEvent.key === "ArrowUp") {
-							keyDownEvent.preventDefault();
-							lastSubTriggerRef.current?.focus();
-						}
-						keyDownEvent.stopPropagation();
+			<DropdownMenu.Portal>
+				<DropdownMenu.Content
+					align="start"
+					className="border border-gray-300 w-[260px] rounded bg-white shadow-md data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=top]:animate-slideDownAndFade"
+					sideOffset={25}
+					alignOffset={0}
+					onCloseAutoFocus={(event) => {
+						event.preventDefault();
+						buttonRef.current?.focus();
 					}}
-					onChange={(e) => setSearchText(e.target.value)}
-					value={searchText}
-					placeholder="Search..."
-					className="border-l-0 focus-visible:ring-offset-0 ring-offset-0 border-r-0 border-t-0 rounded-none focus-visible:ring-0 flex h-11 w-full bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
-				/>
-				{subItemsToRender.map((subItem, index) => (
-					<FilterDropdownCategory
-						after={newFilterCreatedAtCutoff}
-						key={subItem.id}
-						category={subItem}
-						triggerText={subItem.label}
-						onKeyDown={
-							index === 0
-								? firstCategoryOnKeyDown
-								: index === subItemsToRender.length - 1
-									? lastCategoryOnKeyDown
-									: undefined
-						}
-						ref={
-							index === 0
-								? firstSubTriggerRef
-								: index === subItemsToRender.length - 1
-									? lastSubTriggerRef
-									: undefined
-						}
+				>
+					<Input
+						id="search-input"
+						onKeyDown={(keyDownEvent) => {
+							if (keyDownEvent.key === "ArrowDown") {
+								keyDownEvent.preventDefault();
+								firstSubTriggerRef.current?.focus();
+							} else if (keyDownEvent.key === "ArrowUp") {
+								keyDownEvent.preventDefault();
+								lastSubTriggerRef.current?.focus();
+							}
+							keyDownEvent.stopPropagation();
+						}}
+						onChange={(e) => setSearchText(e.target.value)}
+						value={searchText}
+						placeholder="Search..."
+						className="border-l-0 focus-visible:ring-offset-0 ring-offset-0 border-r-0 border-t-0 rounded-none focus-visible:ring-0 flex h-11 w-full bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
 					/>
-				))}
-				{subItemsToRender.length === 0 ? (
-					<div className="p-4 text-sm text-muted-foreground grid place-items-center">
-						No results found.
-					</div>
-				) : null}
-			</DropdownMenu.Content>
+					{subItemsToRender.map((subItem, index) => (
+						<FilterDropdownCategory
+							after={newFilterCreatedAtCutoff}
+							key={subItem.id}
+							category={subItem}
+							triggerText={subItem.label}
+							onKeyDown={
+								index === 0
+									? firstCategoryOnKeyDown
+									: index === subItemsToRender.length - 1
+										? lastCategoryOnKeyDown
+										: undefined
+							}
+							ref={
+								index === 0
+									? firstSubTriggerRef
+									: index === subItemsToRender.length - 1
+										? lastSubTriggerRef
+										: undefined
+							}
+						/>
+					))}
+					{subItemsToRender.length === 0 ? (
+						<div className="p-4 text-sm text-muted-foreground grid place-items-center">
+							No results found.
+						</div>
+					) : null}
+				</DropdownMenu.Content>
+			</DropdownMenu.Portal>
 		</>
 	);
 };
